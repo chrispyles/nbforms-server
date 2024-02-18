7f19fd7954bfb749220f8b2841ea380363471039
Chris Pyles <cpyles@berkeley.edu>
2024-02-18 12:43:15 -0800

    clear group tests

--- test/test_cli.py
+++ test/test_cli.py
@@ -5,7 +5,7 @@ import pytest
 from click.testing import CliRunner, Result
 
 from nbforms_server.__main__ import cli
-from nbforms_server.models import AttendanceSubmission, db, Notebook, Response
+from nbforms_server.models import AttendanceSubmission, db, Notebook, Response, User
 
 
 def assert_cli_result(result: Result, expect_error, want_stdout=None, want_exc=None):
@@ -84,3 +84,108 @@ class TestAttendance:
       nb = db.session.query(Notebook).filter_by(identifier=args[0]).first()
 
     assert nb.attendance_open is False
+
+
+class TestClear:
+  """Tests for the ``clear`` group."""
+
+  @pytest.mark.parametrize(("force", "confirm", "want_clear"), (
+    (False, "", False),
+    (False, "n", False),
+    (False, "y", True),
+    (False, "Y", True),
+    (True, None, True),
+  ))
+  def test_all(
+    self,
+    app,
+    run_cli,
+    seed_data,
+    seed_responses,
+    seed_attendance_submissions,
+    force,
+    confirm,
+    want_clear,
+  ):
+    """Test the ``clear all`` command."""
+
+    res = run_cli(["clear", "all"] + (["--force"] if force else []), input=confirm)
+    assert_cli_result(res, False, None, None)
+
+    with app.app_context():
+      assert len(db.session.query(Response).all()) == (0 if want_clear else 9)
+      assert len(db.session.query(AttendanceSubmission).all()) == (0 if want_clear else 4)
+
+  @pytest.mark.parametrize(("username", "force", "confirm", "want_clear", "want_exc"), (
+    ("anakin", False, "", False, None),
+    ("anakin", False, "n", False, None),
+    ("anakin", False, "y", True, None),
+    ("anakin", False, "Y", True, None),
+    ("anakin", True, None, True, None),
+    # nonexistant user
+    ("han", False, "y", False, ValueError("No such user: han")),
+  ))
+  def test_user(
+    self,
+    app,
+    run_cli,
+    seed_responses,
+    seed_attendance_submissions,
+    username,
+    force,
+    confirm,
+    want_clear,
+    want_exc,
+  ):
+    """Test the ``clear user`` command."""
+    with app.app_context():
+      seed_usernames = [u.username for u in db.session.query(User).all()]
+
+    res = run_cli(["clear", "user", username] + (["--force"] if force else []), input=confirm)
+    assert_cli_result(res, username not in seed_usernames, None, want_exc)
+
+    if username in seed_usernames:
+      with app.app_context():
+        res, sub = (
+          db.session.query(Response).join(User).filter(User.username == username).all(),
+          db.session.query(AttendanceSubmission).join(User).filter(User.username == username).all(),
+        )
+        assert len(res) == (0 if want_clear else 3)
+        assert len(sub) == (0 if want_clear else 1)
+
+  @pytest.mark.parametrize(("notebook", "force", "confirm", "want_clear", "want_exc"), (
+    ("naboo", False, "", False, None),
+    ("naboo", False, "n", False, None),
+    ("naboo", False, "y", True, None),
+    ("naboo", False, "Y", True, None),
+    ("naboo", True, None, True, None),
+    # nonexistant notebook
+    ("mustafar", False, "y", False, ValueError("No such notebook: mustafar")),
+  ))
+  def test_notebook(
+    self,
+    app,
+    run_cli,
+    seed_responses,
+    seed_attendance_submissions,
+    notebook,
+    force,
+    confirm,
+    want_clear,
+    want_exc,
+  ):
+    """Test the ``clear notebook`` command."""
+    with app.app_context():
+      seed_notebooks = [n.identifier for n in db.session.query(Notebook).all()]
+
+    res = run_cli(["clear", "notebook", notebook] + (["--force"] if force else []), input=confirm)
+    assert_cli_result(res, notebook not in seed_notebooks, None, want_exc)
+
+    if notebook in seed_notebooks:
+      with app.app_context():
+        res, sub = (
+          db.session.query(Response).join(Notebook).filter(Notebook.identifier == notebook).all(),
+          db.session.query(AttendanceSubmission).join(Notebook).filter(Notebook.identifier == notebook).all(),
+        )
+        assert len(res) == (0 if want_clear else 6)
+        assert len(sub) == (0 if want_clear else 4)
